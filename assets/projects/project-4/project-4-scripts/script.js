058cfaa6f4ff6b941405c08300b2557f435b45ca
qcuincy <quincy.sproul@gmail.com>
2023-09-07 17:45:52 +0100

    Fixed bug causing page refresh on window resize

---
+++ assets/projects/project-4/project-4-scripts/script.js
@@ -0,0 +1,12 @@
+let  swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop:true,
+  navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+  },
+});
